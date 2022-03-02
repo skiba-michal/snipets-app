09fb406200e2fbf08dda98327acb2c8ae700f3f7
Michał Skiba <michal.skiba@euvic.pl>
2022-03-02 19:46:33 +0100

    setup env

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,20 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const url = process.env.REACT_APP_URL || process.env.PUBLIC_URL;
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    fetch(`${url}/about`)
+      .then((response) => response.json())
+      .then((data) => setValue(data));
+  };
   return (
     <div className="App">
       <header className="App-header">
+        {value}
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
--- server.js
+++ server.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 console.log("try");
 
+
+app.use(express.static(path.join("public")));
+
 app.use((req, res, next) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); mozna wyjebac
   next();
 });
 
 app.get("/about", function (req, res) {
   console.log("eloooo");
-  res.send("about");
+  res.send(JSON.stringify("abouttttt"));
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use((req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 mongoose
-  .connect("mongodb+srv://user:user@cluster0.0unqv.mongodb.net/snipet?retryWrites=true&w=majority")
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0unqv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
   .then((res) => {
-    app.listen(8080, () => {
+    app.listen(process.env.PORT || 8080, () => {
       console.log("poszlo");
     });
   })
